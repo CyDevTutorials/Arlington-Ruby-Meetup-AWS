8ad69362b5d84a013670e1d696ae310a5a5dbe5e
E Dunson <etdunson@gmail.com>
2014-02-21 22:12:15 -0500

    First Commit for Arlington Ruby Meetup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubySample::Application.config.secret_key_base = 'fc1ebbd4585ec012c16f0ae2381ff7f6f178a0adf5fc539167bbc27a1e6768f71014ba5029347e819c6bc06f28eb33fce1e9a6f8f8acf03e2bf4b610ffbbb7b6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubySample::Application.config.session_store :cookie_store, key: '_ruby-sample_session'
