cc11a17d7a7810726309d7a963b74bbf061e7a27
E Dunson <etdunson@gmail.com>
2014-02-23 22:55:29 -0500

    changed configuration details

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 RubySample::Application.routes.draw do
   
-  root :to => 'home#index';  
+  root :to => 'home#index' 
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
